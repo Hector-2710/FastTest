06b6f344580e7a160deafe7bd82071821d12789d
hector <h.rosales01@ufromail.cl>
2026-02-24 14:17:53 -0300

    feat: delete body msg

--- utils.py
+++ utils.py
@@ -15,18 +15,4 @@ def print_report(result: str, url: str, status: int, body: str, error_msg: str =
         if error_msg:
             print(f"{bold}Error:{reset}  {error_msg}")
         
-        print(f"{bold}Body:{reset}")
-        try:
-            if isinstance(body, (dict, list)):
-                content = json.dumps(body, indent=4, ensure_ascii=False)
-                print(content)
-            else:
-                try:
-                    loaded = json.loads(body)
-                    print(json.dumps(loaded, indent=4, ensure_ascii=False))
-                except:
-                    print(body)
-        except:
-            print(body)
-            
         print(f"{color}{'='*60}{reset}\n")
