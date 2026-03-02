82c806e7550a9118d84d6018a85cd9255da681e3
hector <h.rosales01@ufromail.cl>
2026-03-01 23:08:48 -0300

    feat: delete basic endpoints

--- api.py
+++ api.py
@@ -9,14 +9,6 @@ list_users = []
 def get_hello():
     return {"Hello": "World"}
 
-@app.get("/post/{id}", status_code=status.HTTP_200_OK)
-def get_post_by_id(id: UUID):
-    return {"id": id}
-
-@app.get("/post", status_code=status.HTTP_200_OK)
-def get_post(id: UUID):
-    return {"id": id}
-
 @app.post("/user", status_code=status.HTTP_201_CREATED)
 def create_user(user: dict):
     list_users.append(user)
